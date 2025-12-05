912eff2f033bda408c66df1f6415c19458565e7a
davberamo <dberlinches2003@gmail.com>
2025-12-05 20:37:06 +0100

    Mejorado los tests unitarios

--- app/src/test/java/com/example/conversorsimple/ConversorViewModelTest.kt
+++ app/src/test/java/com/example/conversorsimple/ConversorViewModelTest.kt
@@ -1,36 +1,47 @@
 package com.example.conversorsimple
-import org.junit.Assert.assertEquals
+
+import org.junit.Assert.*
 import org.junit.Test
 
-
 class ConversorViewModelTest {
+
     private val viewModel = ConversorViewModel()
 
+    // CASOS DE ÉXITO (happy path)
     @Test
-    fun testCelsiusToFahrenheit() {
-        viewModel.onValueChange("10")
+    fun convertirCelsiusAFahrenheit() {
+        viewModel.onValueChange("0")
         viewModel.convertToFarenheit()
-
-        assertEquals("50.0ºF", viewModel.resultado.value)
+        assertEquals("32.0ºF", viewModel.resultado.value)
     }
+
     @Test
-    fun testFahrenheitToCelsius() {
+    fun convertirFahrenheitACelsius() {
         viewModel.onValueChange("32")
         viewModel.convertToCelsius()
         assertEquals("0.0ºC", viewModel.resultado.value)
     }
 
+    // CASOS DE ERROR (error path)
     @Test
-    fun testNegativeCelsiusToFahrenheit() {
-        viewModel.onValueChange("-40")
-        viewModel.convertToFarenheit()
-        assertEquals("-40.0ºF", viewModel.resultado.value)
-    }
-
-    @Test
-    fun testEntradaInvalida() {
+    fun entradaInvalidaDevuelveMensajeError() {
         viewModel.onValueChange("abc")
         viewModel.convertToFarenheit()
         assertEquals("Entrada inválida", viewModel.resultado.value)
+    }
+
+    // CASOS LÍMITE (boundary case)
+    @Test
+    fun convertirMenos40CelsiusAFahrenheit() {
+        viewModel.onValueChange("-40")
+        viewModel.convertToFarenheit()
+        assertEquals("-40.0ºF", viewModel.resultado.value) // caso especial: -40 es igual en ambas escalas
+    }
+
+    @Test
+    fun convertirMenos40FahrenheitACelsius() {
+        viewModel.onValueChange("-40")
+        viewModel.convertToCelsius()
+        assertEquals("-40.0ºC", viewModel.resultado.value)
     }
 }
